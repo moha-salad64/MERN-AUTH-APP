bbe652d72c2c5a245baab9e05dbb7b050bc8d8d0
moha-salad64 <mohamedsalaad509@gmail.com>
2025-03-24 00:24:18 +0300

    creating user model

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const {config} = require('dotenv')
 const connectionDB = require('./database/db');
 
+const userRoute = require('./routes/userRoute')
+
 //load env variables 
 config();
 //database connection 
@@ -10,9 +12,15 @@ connectionDB()
 const app = express();
 const PORT = process.env.PORT || 5151
 
-app.get('/' , (req , res) =>{
-    res.send('wellcome to my server')
-})
+
+app.use(express.json());
+
+// app.get('/' , (req , res) =>{
+//     res.send('wellcome to my server')
+// })
+
+
+app.use('api/' , userRoute)
 
 app.listen(PORT , () =>{
     console.log(`this server running at this ${PORT} port`)

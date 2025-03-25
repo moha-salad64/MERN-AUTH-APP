f042b995d96f4500b9f862928ae6cc8f1c846a54
moha-salad64 <mohamedsalaad509@gmail.com>
2025-03-25 23:12:17 +0300

    making password hashing

--- server/controllers/aurthController.js
+++ server/controllers/aurthController.js
@@ -1,17 +1,25 @@
 const User = require('../model/userModel')
+const bcryptjs = require('bcryptjs')
 
 exports.signUpController = async (req , res) =>{
 //    console.log(req.body)
-    const {username , email , password} = req.body;
-    
-    const newUser = new User({
+    try {
+
+        const {username , email , password} = req.body; //handling the requist from the body
+        const hashedPassword = bcryptjs.hashSync(password , 10); // encrypting password or hashing
+        const newUser = new User({ // storing data from requist body
         username,
         email,
-        password
+        password : hashedPassword
     })
 
-    await newUser.save();
-    res.status(201).json({
-        message:"user creation successfully!"
-    })
+        await newUser.save(); //saving data to the database
+        res.status(201).json({
+            message:"user creation successfully!"
+        })
+        
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+    
 }

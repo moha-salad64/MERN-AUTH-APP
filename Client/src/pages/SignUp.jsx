393e8816d1c6f4a23d847ec70eea6803b76f5745
moha-salad64 <mohamedsalaad509@gmail.com>
2025-03-26 02:06:29 +0300

    creating sign up design

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-    <h1 className='text-4xl text-center font-bold my-7'>Sign Up</h1>
+    <div className='p-6 mt-6 max-w-lg mx-auto shadow-xl shadow-blue-500/50 rounded-2xl bg-gray-950 text-white'>
+    <h1 className='text-4xl text-center font-bold mt-7'>Sign Up</h1>
 
     <form className='flex flex-col gap-4'>
     <div className='flex flex-col m-1'>
-      <label  className='text-2xl text-uppercase font-bold capitalize'>username</label>
+      <label  className='text-xl text-uppercase font-bold capitalize'>username</label>
     <input 
       type='text'
-      placeholder='username'
-       className='bg-slate-100 p-2 my-1 rounded-lg outline-0 '
+      placeholder='Username'
+      className='bg-slate-100 p-2 my-1 rounded-lg outline-0 hover:border border-blue-800m font-medium text-black'
     />
     </div>
 
     <div className='flex flex-col m-1'>
-      <label className='text-2xl text-uppercase font-bold capitalize'>Email</label>
+      <label className='text-xl text-uppercase font-bold capitalize'>Email</label>
     <input 
       type='email'
       placeholder='Email'
-      className='bg-slate-100 p-2 my-1 rounded-lg outline-0'
+      className='bg-slate-100 p-2 my-1 rounded-lg outline-0 hover:border border-blue-800 font-medium text-black'
     />
     </div>
 
@@ -29,15 +30,23 @@ export default function SignUp() {
     <input 
       type='password'
       placeholder='Password'
-      className='bg-slate-100 p-2 my-1 rounded-lg outline-0'
+      className='bg-slate-100 p-2 my-1 rounded-lg outline-0 hover:border border-blue-800 font-medium text-black'
     />
     </div>
 
-    <div className='flex flex-col'>
-      <button type='submit' className='bg-slate-700 text-white p-2 rounded-lg hover:opacity-95 disabled:opacity-80 uppercase font-semibold text-1xl'>sign up</button>
-      <button type='submit' className='bg-red-600 text-white p-2 uppercase rounded-lg my-2 hover:opacity-95 disabled:opacity-80 font-semibold text-1xl'>continue with google</button>
+    <div className='flex flex-col my-2'>
+      <button type='submit' className='bg-blue-700 text-white p-2 rounded-lg hover:opacity-95 disabled:opacity-80 uppercase font-semibold text-1xl cursor-pointer'>sign up</button>
+      <button type='submit' className='bg-slate-800 text-white p-2 uppercase rounded-lg mt-3 hover:opacity-95 disabled:opacity-80 font-semibold text-1xl cursor-pointer'>continue with google</button>
     </div>
     </form>
+
+    <div className='flex gap-2 mt-4'>
+    <p className='font-medium'>Having a account?</p>
+    <Link to="/signin">
+    <span className='text-blue-700 font-medium'>Sign in</span>
+    </Link>
+    </div>
+
     </div>
   )
 }
